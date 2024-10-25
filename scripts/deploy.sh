3851c73949c708ddaa964eb4813a587e6ad0604b
devtoc89 <devtoc89@gmail.com>
2024-10-25 18:20:55 +0900

    fix: move static file to src and deploy file

---
+++ scripts/deploy.sh
@@ -0,0 +1,16 @@
+PROJECT_ROOT="/home/ec2-user/project/forduellee_html"
+SRC_DEPLOY_FOLDER="$PROJECT_ROOT/src"
+ORG_DEPLOY_FOLDER="/usr/share/nginx/html"
+
+APP_LOG="$PROJECT_ROOT/application.log"
+ERROR_LOG="$PROJECT_ROOT/error.log"
+DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
+
+TIME_NOW=$(date +%c)
+
+# build 파일 복사
+echo "$TIME_NOW > 파일 복사" >> $DEPLOY_LOG
+sudo rm -rf $ORG_DEPLOY_FOLDER
+sudo cp -r $SRC_DEPLOY_FOLDER $ORG_DEPLOY_FOLDER
+sudo chmod -R nginx:nginx $ORG_DEPLOY_FOLDER
+
---
+++ scripts/remove.sh
@@ -0,0 +1,2 @@
+PROJECT_ROOT="/home/ec2-user/project/forduellee_html"
+sudo rm -rf $PROJECT_ROOT
